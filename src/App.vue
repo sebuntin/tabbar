78f253a5c3fb5c81bf955b8c46b91043a01436d4
sebuntin <kirito331789567@gmail.com>
2022-01-03 21:42:02 +0800

    TabBar和TabItem组件封装

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,70 @@
 <template>
+  <div id="app">
+    <tab-bar>
+      <tab-bar-item>
+        <img
+          slot="item-icon"
+          src="./assets/img/tabbar/home-outline.svg"
+          alt=""
+        >
+        <img
+          slot="item-icon-active"
+          src="./assets/img/tabbar/home-active.svg"
+          alt=""
+        >
+        <div slot="item-text">首页</div>
+      </tab-bar-item>
+      <tab-bar-item>
+        <img
+          slot="item-icon"
+          src="./assets/img/tabbar/cart-outline.svg"
+          alt=""
+        >
+        <img
+          slot="item-icon-active"
+          src="./assets/img/tabbar/cart-active.svg"
+          alt=""
+        >
+        <div slot="item-text">购物车</div>
+      </tab-bar-item>
+      <tab-bar-item>
+        <img
+          slot="item-icon"
+          src="./assets/img/tabbar/grid-outline.svg"
+          alt=""
+        >
+        <img
+          slot="item-icon-active"
+          src="./assets/img/tabbar/grid-active.svg"
+          alt=""
+        >
+        <div slot="item-text">更多</div>
+      </tab-bar-item>
+      <tab-bar-item>
+        <img
+          slot="item-icon"
+          src="./assets/img/tabbar/person-outline.svg"
+          alt=""
+        >
+        <img
+          slot="item-icon-active"
+          src="./assets/img/tabbar/person-active.svg"
+          alt=""
+        >
+        <div slot="item-text">我的</div>
+      </tab-bar-item>
+    </tab-bar>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TabBar from './components/tabbar/tabBar.vue'
+import TabBarItem from './components/tabbar/tabBarItem.vue'
 export default {
   name: 'App',
   components: {
+    TabBar,
+    TabBarItem
   }
 }
 </script>
---
+++ src/components/tabbar/tabBarItem.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="tab-bar-item">
+    <slot
+      v-if="!isActive"
+      name='item-icon'
+    ></slot>
+    <slot
+      v-else
+      name='item-icon-active'
+    ></slot>
+    <div :class="{active: isActive}">
+      <slot name='item-text'></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TabBarItem',
+  data() {
+    return {
+      isActive: true 
+    }
+  },
+}
+</script>
+
+<style>
+.tab-bar-item {
+  flex: 1;
+  text-align: center;
+  height: 49px;
+  color: dimgray;
+}
+.tab-bar-item img {
+  width: 24px;
+  height: 24px;
+  margin-top: 3px;
+  vertical-align: middle;
+}
+.active {
+  color: black;
+}
+</style>
